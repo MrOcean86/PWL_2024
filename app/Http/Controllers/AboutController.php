76b042e6d54a34a3917cf4d10948eee694fb5923
MrOcean86 <muhammadalimul86@gmail.com>
2025-03-19 01:50:44 +0700

    Menambahkan file dasar untuk proyek Laravel, termasuk konfigurasi, pengaturan lingkungan, dan struktur direktori. File-file baru mencakup .editorconfig, .env.example, .gitattributes, .gitignore, dan berbagai file konfigurasi serta migrasi untuk database. Juga menambahkan file pengujian dan contoh untuk memulai pengembangan.

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about()
+    {
+        return 'Nama: Muhammad Alimul Hakim <br>NIM: 23552021004';
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
+
+
+
+Route::get('/', [HomeController::class,'index']);
+Route::get('/about', [AboutController::class,'about']);
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
+Route::get('/greeting', [WelcomeController::class,'greeting']);
+
+
+Route::resource('photos', PhotoController::class)->only([
+    'index','show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+    'create','store','update','destroy'
+]);
+
